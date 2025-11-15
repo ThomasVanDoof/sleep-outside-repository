d0a6e3a286b9f260bb314db7ab5d9184f9ad50b6
yourusernameCave <dracothedragon9292@gmail.com>
2025-11-14 19:30:09 -0700

    fixing add to cart array, and making a remove from cart button

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,24 +2,18 @@
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+  return JSON.parse(localStorage.getItem(key)) || [];
 }
+
 // save data to local storage
 export function setLocalStorage(key, data) {
-  // retrieve any existing data
-  let existing = localStorage.getItem(key);
-  let arr = existing ? JSON.parse(existing) : [];
-  //convert to array
-  if (!Array.isArray(arr)) arr = [arr];
-  arr.push(data);
-  localStorage.setItem(key, JSON.stringify(arr));
+  localStorage.setItem(key, JSON.stringify(data));
 }
-// set a listener for both touchend and click
+
+
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
     event.preventDefault();
